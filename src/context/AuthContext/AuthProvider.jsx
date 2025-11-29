88abf63022bf766474075d40f21a5ba24f6ebd40
Nobodip Debnath <nobodipdebnath45@gmail.com>
2025-11-29 19:07:19 +0600

    add some file

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
+import axios from 'axios';
 
 const AuthProvider = ({children}) => {
     const [loading, setLoading] = useState(true);
@@ -30,6 +31,17 @@ const AuthProvider = ({children}) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             setLoading(false);
+            if(currentUser?.email){
+                const userData = {email: currentUser.email};
+                axios.post('http://localhost:3000/jwt', userData, {
+                    withCredentials: true
+                })
+                .then(res => {
+                    console.log(res.data);
+                })
+                .catch(error => console.log(error));
+            }
+            console.log('user in the auth state', currentUser)
         })
         return () => {
             unSubscribe();
